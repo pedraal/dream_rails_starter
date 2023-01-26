0aee641d010f763a322b3fafbfdd2b6cbff4275f
Pierre Golfier <pgolfier.pro@gmail.com>
2023-01-26 21:16:19 +0100

    feat: install bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import Rails from 'vite-plugin-rails'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [
     Rails(),
+    visualizer(),
   ],
 })
